ccc929db6b216f085cd069c6a953fbba6bd4e3e5
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-03-14 21:05:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+from pyrogram import Client, filters
 
+Bot=Client(
+    "Bot",
+    bot_token="5125014420:AAEK2E9aj-q9FHZknW_hrisJvCuONx0XPtU",
+    api_id="17946666",
+    api_hash="d9647913e97bf2f6a66d978290284028"
+)
+
+@Bot.on_message(filters.("start"))
+async def start_message(bot, message):
+    await message.reply_text("mission passed")
+
+
+Bot.run()
